056a2d43c2abb011d98c89785436685f24517daf
Matteo Lucerni <zzzmat01.luce@gmail.com>
2023-03-22 19:48:20 +0100

    updated

---
+++ ProgettoWebDev/script.js
@@ -0,0 +1,15 @@
+const BackBtn = document.getElementById('backBtn');
+
+backBtn.addEventListener('click', function(){
+    window.scrollTo(0,0)
+});
+
+window.addEventListener('scroll', function(){
+    let posY = window.scrollY
+
+    if(posY > 100){
+        backBtn.style.display = 'block'
+    } else {
+        backBtn.style.display = 'none'
+    }
+});
